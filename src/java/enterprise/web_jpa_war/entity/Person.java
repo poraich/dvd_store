2729ea7c651ac229f107ee2977b3e48d2830e433
Katie <Katie@Katie-PC.wireless.l3.nuigalway.ie>
2013-11-15 15:46:52 +0000

    email

--- src/java/enterprise/web_jpa_war/entity/Person.java
+++ src/java/enterprise/web_jpa_war/entity/Person.java
@@ -1,9 +1,36 @@
-
+/*
+ * Copyright (c) 2010, Oracle. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Oracle nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 
 package enterprise.web_jpa_war.entity;
 
-//import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -19,65 +46,44 @@ public class Person {
 
     @Id
     @Column(name = "ID")
-    private String cID;
+    private String id;
 
     @Column(name = "LASTNAME")
-    private String lName;
+    private String lastName;
 
     @Column(name = "FIRSTNAME")
-    private String fName;
-     
-    @Column(name = "E-mail")
-    private String email;
+    private String firstName;
     
-     @Column(name = "DOB")
-    private String date;
-     
-      @Column(name = "Password")
-    private String pass;
-      
-       @Column(name = "Address")
-    private String add;
+    @Column(name = "EMAIL")
+    private String email;
 
-    /** 
+    /**
      * Creates a new instance of Person
      */
     public Person() {
     }
 
-    public Person(String id, String firstName, String lastName, String em,String d,String pw,String a) {
-        this.cID = id;
-        this.fName = firstName;
-        this.lName  = lastName;
-        this.email=em;
-        this.date=d;
-        this.pass=pw;
-        this.add=a;
-        
+    public Person(String id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName  = lastName;
+        this.email = email;
     }
 
     public String getId() {
-        return this.cID;
+        return this.id;
     }
 
     public String getLastName() {
-        return this.lName;
+        return this.lastName;
     }
 
     public String getFirstName() {
-        return this.fName;
+        return this.firstName;
     }
-    public String getemail() {
+    
+     public String getEmail() {
         return this.email;
-    }
-    public String getDOB() {
-        return this.date;
-    }
-    public String getPW() {
-        return this.pass;
-    }
-    public String getAddress() {
-        return this.add;
     }
     
 }
